c7a7c18fd8a64f27e9cb38533f36fea8e51cc158
hsov9314 <hsov3149@gmail.com>
2023-02-21 00:31:32 +0900

    [fix] set sidebar sticky

--- app/src/components/SideBar/SideBar.tsx
+++ app/src/components/SideBar/SideBar.tsx
@@ -3,7 +3,7 @@ import { SideBarItem } from './SideBarItem'
 
 export const SideBar = () => {
   return (
-    <div className="w-60 py-4 bg-slate-700">
+    <div className="w-60 py-4 bg-slate-700 fixed top-0 left-0 h-full">
       <SideBarHeader label="ダッシュボード" />
       {['概要', 'データ', '設定'].map((item) => (
         <SideBarItem label={item} />
